2a02dd1b74094718728499495a7314d711bb6024
ZensykU2 <crto.CT@gmail.com>
2025-09-15 16:10:40 +0200

    Aufgabe 1 & 2

--- BlockEins/Program.cs
+++ BlockEins/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+// Aufgabe 1
+/* Console.WriteLine("Bitte schreibe deinen Namen");
+string name = Console.ReadLine();
+Console.WriteLine("Hallo " + name + "! Schön, dass du da bist.");
+*/
+
+//Aufgabe 2
+/*Console.WriteLine("Bitte gib die erste Zahl ein:");
+string num1 = Console.ReadLine();
+Console.WriteLine("Bitte gib nun die zweite Zahl ein:");
+string num2 = Console.ReadLine();
+int sumNum1 = int.Parse(num1);
+int sumNum2 = int.Parse(num2);
+int sum = sumNum1 + sumNum2;
+Console.WriteLine("Hier ist nun die Summe der zwei Zahlen: " + sum);
+*/
